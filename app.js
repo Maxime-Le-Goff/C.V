386d7947bbb971e21b13255c2e18701a0b37a314
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2023-01-07 17:35:07 +0100

    wip stats bar

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 const link = document.getElementById('link')
 const burger = document.getElementById('burger')
 const ul = document.querySelector('ul')
-// let progress = document.querySelector('.check-competences');
-// progress.addEventListener('click',pbclick)
+window.addEventListener('DOMContentLoaded',progressBar)
 
 link.addEventListener('click', function(e) {
   e.preventDefault()
   burger.classList.toggle('open')
   ul.classList.toggle('open')
 })
-
+function progressBar(){
+  console.log('oui')
+  let pb = document.getElementById('pb-rigueur');
+  console.log(pb)
+  pb.style.transition= "width 5s ease-in-out 0s"
+}
 // function pbclick(e){
 //   var elem = document.getElementById("pb-rigueur");
 //   var width = 1;
