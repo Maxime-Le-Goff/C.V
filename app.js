3cdf37a46b3c041326a334a9e53c96e2e610f73f
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2023-01-25 15:37:53 +0100

    progress bar +footer

--- app.js
+++ app.js
@@ -2,29 +2,41 @@
 const link = document.getElementById('link')
 const burger = document.getElementById('burger')
 const ul = document.querySelector('ul')
-window.addEventListener('DOMContentLoaded',progressBar)
 
 link.addEventListener('click', function(e) {
   e.preventDefault()
   burger.classList.toggle('open')
   ul.classList.toggle('open')
 })
-function progressBar(){
-  console.log('oui')
-  let pb = document.getElementById('pb-rigueur');
-  console.log(pb)
-  pb.style.transition= "width 5s ease-in-out 0s"
+
+const bars = document.querySelectorAll('.progress');
+bars.forEach(item =>{
+  let value = item.id;
+  let contador = 0;
+  let interval = setInterval(function(){
+    item.style.width = contador + "%";
+    if(contador == value){
+      clearInterval(interval);
+    }
+    contador++;
+  },8);
+})
+
+const text = document.querySelector(".sec-text");
+console.log(text);
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = 'Rigoureux';
+  },0);
+  setTimeout(() => {
+    text.textContent = 'Passionné';
+  },4000);
+  setTimeout(() => {
+    text.textContent = 'Joyeux';
+  },8000);
+  setTimeout(() => {
+    text.textContent = 'Bonne Humeur';
+  },12000);
 }
-// function pbclick(e){
-//   var elem = document.getElementById("pb-rigueur");
-//   var width = 1;
-//   var id = setInterval(frame, 10);
-//   function frame() {
-//     if (width >= 100) {
-//       clearInterval(id);
-//     } else {
-//       width++;
-//       elem.style.width = width + '%';
-// }
-//   }
-// }
+textLoad();
+setInterval(textLoad,16000);
