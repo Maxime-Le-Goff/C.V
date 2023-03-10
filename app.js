f34d03a9d045fa1d6c6d1b82c485590128262c62
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2023-03-10 16:59:45 +0100

    integration done

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-/* Sélection des éléments HTML */
-const link = document.getElementById('link')
-const burger = document.getElementById('burger')
-const ul = document.querySelector('ul')
-
-link.addEventListener('click', function(e) {
-  e.preventDefault()
-  burger.classList.toggle('open')
-  ul.classList.toggle('open')
-})
+const text = document.querySelector(".sec-text");
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = 'RIGOUREUX';
+  },0);
+  setTimeout(() => {
+    text.textContent = 'PASSIONNE';
+  },3000);
+  setTimeout(() => {
+    text.textContent = 'CURIEUX';
+  },6000);
+}
+textLoad();
+setInterval(textLoad,9000);
 
 const bars = document.querySelectorAll('.progress');
 bars.forEach(item =>{
@@ -22,31 +26,12 @@ bars.forEach(item =>{
   },8);
 })
 
-const text = document.querySelector(".sec-text");
-console.log(text);
-const textLoad = () => {
-  setTimeout(() => {
-    text.textContent = 'RIGOUREUX';
-  },0);
-  setTimeout(() => {
-    text.textContent = 'PASSIONNE';
-  },3000);
-  setTimeout(() => {
-    text.textContent = 'JOYEUX';
-  },6000);
-  setTimeout(() => {
-    text.textContent = 'LOREM';
-  },9000);
-}
-textLoad();
-setInterval(textLoad,12000);
+
 
 const card = document.querySelector('.Card_card');
 const itemCard = document.querySelector('.Card_add i');
-console.log(itemCard);
 document.querySelector('.Card_add').addEventListener('click',() => {
   card.classList.toggle('Card_on');
-  console.log(itemCard);
   if(itemCard.classList.contains('fa-minus')){
     itemCard.classList.replace('fa-minus','fa-plus');
   }
@@ -54,3 +39,12 @@ document.querySelector('.Card_add').addEventListener('click',() => {
     itemCard.classList.replace('fa-plus','fa-minus');
   }
 })
+
+function Burger() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
